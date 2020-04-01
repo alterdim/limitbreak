90602eda2d96b14c3f229aa20d001578109edbd7
Alternis <roilouisdim@gmail.com>
2020-04-01 17:04:59 +0200

    glorious statue

--- src/main/java/com/alterdim/limitbreak/init/BlockInit.java
+++ src/main/java/com/alterdim/limitbreak/init/BlockInit.java
@@ -2,6 +2,7 @@ package com.alterdim.limitbreak.init;
 
 import com.alterdim.limitbreak.LimitBreak;
 import com.alterdim.limitbreak.LimitBreak.LimitBreakItemGroup;
+import com.alterdim.limitbreak.objects.blocks.GloriousStatue;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -10,6 +11,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,18 +24,22 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 public class BlockInit 
 {
 	public static final Block gayming_block = null;
+	public static final Block glorious_statue = null;
 	
 	@SubscribeEvent
 	public static void registerBlocks(final RegistryEvent.Register<Block> event)
 	{
 		event.getRegistry().register(new 
-				Block(Block.Properties.create(Material.IRON).hardnessAndResistance(5, 6).harvestLevel(3)
+				Block(Block.Properties.create(Material.IRON).hardnessAndResistance(5, 6).harvestLevel(3).harvestTool(ToolType.PICKAXE)
 						.sound(SoundType.METAL)).setRegistryName("gayming_block"));
+		event.getRegistry().register(new GloriousStatue(Block.Properties.create(Material.GLASS).hardnessAndResistance(3).harvestLevel(0)
+				.sound(SoundType.GLASS)).setRegistryName("glorious_statue"));
 	}
 	
 	@SubscribeEvent
 	public static void registerBlockItems(final RegistryEvent.Register<Item> event)
 	{
 		event.getRegistry().register(new BlockItem(gayming_block, new Item.Properties().group(LimitBreakItemGroup.instance)).setRegistryName("gayming_block"));
+		event.getRegistry().register(new BlockItem(glorious_statue, new Item.Properties().group(LimitBreakItemGroup.instance)).setRegistryName("glorious_statue"));
 	}
 }
---
+++ src/main/java/com/alterdim/limitbreak/objects/blocks/GloriousStatue.java
@@ -0,0 +1,110 @@
+package com.alterdim.limitbreak.objects.blocks;
+
+import java.util.stream.Stream;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+
+public class GloriousStatue extends Block
+{
+	
+	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+	
+	private static final VoxelShape SHAPE_N = Stream.of(
+			Block.makeCuboidShape(3, 0, 3, 13, 1, 13),
+			Block.makeCuboidShape(7, 9, 7, 9, 10, 9),
+			Block.makeCuboidShape(7, 2, 7, 9, 6, 9),
+			Block.makeCuboidShape(9, 1, 7, 11, 3, 9),
+			Block.makeCuboidShape(7, 6, 7, 9, 9, 9),
+			Block.makeCuboidShape(5, 1, 7, 7, 3, 9)
+			).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+	private static final VoxelShape SHAPE_E = Stream.of(
+			Block.makeCuboidShape(3, 0, 3, 13, 1, 13),
+			Block.makeCuboidShape(7, 1, 9, 9, 3, 11),
+			Block.makeCuboidShape(7, 2, 7, 9, 6, 9),
+			Block.makeCuboidShape(7, 6, 7, 9, 9, 9),
+			Block.makeCuboidShape(7, 9, 7, 9, 10, 9),
+			Block.makeCuboidShape(7, 1, 5, 9, 3, 7)
+			).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+	
+	public GloriousStatue(Properties properties) 
+	{
+		super(properties);
+		this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+	}
+	
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		switch(state.get(FACING))
+		{
+		case NORTH:
+			return SHAPE_N;
+		case SOUTH:
+			return SHAPE_N;
+		case EAST:
+			return SHAPE_E;
+		case WEST:
+			return SHAPE_E;
+		default:
+			return SHAPE_N;
+		}
+	}
+	
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) 
+	{
+		return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+	}
+	
+	@Override
+	public BlockState rotate(BlockState state, Rotation rot) 
+	{
+		return state.with(FACING, rot.rotate(state.get(FACING)));
+	}
+	
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirrorIn) 
+	{
+		return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+	}
+	
+	@Override
+	protected void fillStateContainer(Builder<Block, BlockState> builder) 
+	{
+		builder.add(FACING);
+	}
+	
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player,
+			Hand handIn, BlockRayTraceResult p_225533_6_) 
+	{
+		if (!worldIn.isRemote())
+		{
+			ServerWorld serverworld = (ServerWorld)worldIn;
+			LightningBoltEntity lightning = new LightningBoltEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), false);
+			serverworld.addLightningBolt(lightning);
+		}
+		return ActionResultType.SUCCESS;
+	}
+
+}
